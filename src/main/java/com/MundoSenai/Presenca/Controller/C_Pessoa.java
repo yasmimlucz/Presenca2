7245985497ae1853c89d1df0e282a408f4c572b9
Lucas Fernandes Braz <lucas.braz@sistemafiep.org.br>
2023-07-25 18:33:29 -0300

    Initial commit

---
+++ src/main/java/com/MundoSenai/Presenca/Controller/C_Pessoa.java
@@ -0,0 +1,18 @@
+package com.MundoSenai.Presenca.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class C_Pessoa {
+    @GetMapping("/")
+    public String helloWorld(){
+        return "Login/login";
+    }
+
+    @GetMapping("/teste")
+    public String teste(){
+        return "teste";
+    }
+
+}
